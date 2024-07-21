b1dc7b519cb3edbdaba88aa3ea042cf3fed8763f
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-07-21 15:02:36 +0100

    init of pygame environment

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+window_size = (800, 600)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Test project")
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+pygame.quit()
